cb472dea39a7f7ce4094ac8e4afb405b60e99025
highwayhappen <142639975+highwayhappen@users.noreply.github.com>
2024-03-04 10:01:34 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+<title>Pengolahan data Penjualan</title>
+    </head>
+    <body>
+        <header>
+            <h1>Pengolahan Data Sistem Pengolahan Barang</h1>
+        </header>
+        <h4>Menu</h4>
+        <nav>
+            <ul>
+                <li><a href="input-stok-barang-dagangan.php">Input Stok Barang dagangan</a><li>
+                <li><a href="list-barang-dagangan.php">Tampilkan data barang dagangan</a><li>
+                <li><a href="penjualan-barang-dagangan.php">Penjualan Barang dagangan</a><li>
+                <li><a href="laporan-pembelian.php">Laporan Pembelian</a><li>
+                <li><a href="laporan-penjualan.php">Laporan Penjualan</a><li>
+            </ul>
+        </nav>
+    </body>
+</html>
